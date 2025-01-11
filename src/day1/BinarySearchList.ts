32f07716ac0f9c41409cc00f6fc029bfcb399bca
Nikhil Gudur <52966758+nikhilgudur@users.noreply.github.com>
2025-01-11 14:55:29 -0500

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,1 +1,19 @@
-export default function bs_list(haystack: number[], needle: number): boolean {}
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let start: number = 0;
+    let end: number = haystack.length - 1;
+
+    while (start <= end) {
+        let mid: number = Math.floor((start + end) / 2);
+
+        if (haystack[mid] === needle) {
+            return true;
+        }
+
+        if (haystack[mid] > needle) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return false;
+}
