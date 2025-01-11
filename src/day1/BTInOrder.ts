469d947b6d71b9bc5a4eeea48a9508a1ea1e9709
Nikhil Gudur <52966758+nikhilgudur@users.noreply.github.com>
2025-01-11 17:08:21 -0500

    bt in order

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,14 @@
 export default function in_order_search(head: BinaryNode<number>): number[] {
+    return search(head);
+}
 
+function search(node: BinaryNode<number>): number[] {
+    if (!node.left && !node.right) {
+        return [node.value];
+    }
+
+    const leftValues = node.left ? search(node.left) : [];
+    const curr = node.value;
+    const rightValues = node.right ? search(node.right) : [];
+    return [...leftValues, curr, ...rightValues];
 }
