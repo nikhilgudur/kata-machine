5f4639e26f0fc3b8705cf9cf77ae9d9a7b46c55d
Nikhil Gudur <52966758+nikhilgudur@users.noreply.github.com>
2025-01-11 15:14:14 -0500

    two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,21 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let i: number = 0;
+    let len: number = breaks.length;
+    let jump: number = Math.floor(Math.sqrt(len));
 
+    for (; i < len; i += jump) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jump;
+
+    for (; i <= i + jump && i < len; i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
