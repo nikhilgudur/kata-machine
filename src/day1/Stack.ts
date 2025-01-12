e5229e0672a57fcd9094353269ac48c3a2da1036
Nikhil Gudur <52966758+nikhilgudur@users.noreply.github.com>
2025-01-12 13:45:28 -0500

    stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,25 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    public stack: T[];
 
     constructor() {
+        this.stack = [];
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        this.stack[this.length] = item;
+        this.length++;
+    }
     pop(): T | undefined {
+        if (this.length) {
+            this.length--;
+            return this.stack.pop();
+        }
 
-}
+        return undefined;
+    }
     peek(): T | undefined {
-
-}
+        return this.stack[this.length - 1];
+    }
 }
