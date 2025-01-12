d7138b3910a218fa49dafe0dca2fce8fa58b8e05
Nikhil Gudur <52966758+nikhilgudur@users.noreply.github.com>
2025-01-12 13:28:36 -0500

    queue implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,25 @@
 export default class Queue<T> {
     public length: number;
-
-    
+    public queue: T[];
 
     constructor() {
+        this.queue = [];
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        this.length = this.queue.unshift(item);
+    }
 
-}
     deque(): T | undefined {
+        if (this.length) {
+            this.length -= 1;
+            return this.queue.pop();
+        }
+        return undefined;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.queue[0];
+    }
 }
