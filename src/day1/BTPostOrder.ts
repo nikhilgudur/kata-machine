95cc89b4fe117efba491765d3654ff1e6a8e5e7c
Nikhil Gudur <52966758+nikhilgudur@users.noreply.github.com>
2025-01-12 19:39:31 -0500

    tree traversals

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,11 @@
 export default function post_order_search(head: BinaryNode<number>): number[] {
+    if (!head.left && !head.right) {
+        return [head.value];
+    }
 
+    const leftValues = head.left ? post_order_search(head.left) : [];
+    const rightValues = head.right ? post_order_search(head.right) : [];
+    const curr = head.value;
+
+    return [...leftValues, ...rightValues, curr];
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,11 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+    if (!head.left && !head.right) {
+        return [head.value];
+    }
 
+    const leftValues = head.left ? pre_order_search(head.left) : [];
+    const rightValues = head.right ? pre_order_search(head.right) : [];
+    const curr = head.value;
+
+    return [curr, ...leftValues, ...rightValues];
 }
